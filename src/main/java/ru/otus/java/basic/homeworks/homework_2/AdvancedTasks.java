82878221f2de64877367785523c06f21ced8c359
SofiaKubo <skuboreva@gmail.com>
2026-04-27 18:01:19 +0200

    feat(homework-2): add balance point check

--- src/main/java/ru/otus/java/basic/homeworks/homework_2/AdvancedTasks.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework_2/AdvancedTasks.java
@@ -9,6 +9,21 @@ public class AdvancedTasks {
         int[] thirdArray = {1, 1, 1, 1, 1};
 
         System.out.println(Arrays.toString(sumArrays(firstArray, secondArray, thirdArray)));
+
+        int[] testArrayFirst = {1, 1, 1, 1, 1, 5};
+        int[] testArraySecond = {5, 3, 4, -2};
+        int[] testArrayThird = {7, 2, 2, 2};
+        int[] testArrayFourth = {9, 4};
+        int[] emptyArray = {};
+        int[] nullArray = null;
+
+        System.out.println(hasBalancePoint(testArrayFirst));
+        System.out.println(hasBalancePoint(testArraySecond));
+        System.out.println(hasBalancePoint(testArrayThird));
+        System.out.println(hasBalancePoint(testArrayFourth));
+        System.out.println(hasBalancePoint(emptyArray));
+        System.out.println(hasBalancePoint(nullArray));
+
     }
 
     public static int[] sumArrays(int[]... arrays) {
@@ -33,4 +48,27 @@ public class AdvancedTasks {
         }
         return result;
     }
+
+    public static boolean hasBalancePoint(int[] numbers) {
+        if (numbers == null || numbers.length < 2) {
+            return false;
+        }
+
+        int sum = 0;
+        int leftSum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            leftSum += numbers[i];
+            int rightSum = sum - leftSum;
+            if (leftSum == rightSum) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
