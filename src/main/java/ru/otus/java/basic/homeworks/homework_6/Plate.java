777dad4f8e1b5e144ff1b86a3781c2dbcb1ee8b2
SofiaKubo <skuboreva@gmail.com>
2026-05-12 15:25:42 +0200

    feat(homework_6): add plate food replenishment method

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework_6/Plate.java
@@ -0,0 +1,28 @@
+package ru.otus.java.basic.homeworks.homework_6;
+
+public class Plate {
+    private final int maxFood;
+    private int currentFood;
+
+    public Plate(int maxFood) {
+        if (maxFood <= 0) {
+            throw new IllegalArgumentException("Plate capacity must be positive");
+        }
+
+        this.maxFood = maxFood;
+        this.currentFood = maxFood;
+    }
+
+    public void addFood(int amount) {
+        if (amount <= 0) {
+            return;
+        }
+
+        if (currentFood + amount > maxFood) {
+            currentFood = maxFood;
+            return;
+        }
+
+        currentFood += amount;
+    }
+}
